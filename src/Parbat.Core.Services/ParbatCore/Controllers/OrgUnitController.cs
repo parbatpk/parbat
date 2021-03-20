4554f31c81aabb3bc9d34f37c083756aa5691848
Ahsankkhan <ahsankkhan88@gmail.com>
2021-03-20 19:19:54 +0500

    Created OrgUnit

---
+++ src/Parbat.Core.Services/ParbatCore/Controllers/OrgUnitController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Parbat.Data;
+using ParbatCore.Models;
+
+namespace ParbatCore.Controllers
+{
+    [Route(GlobalConstants.API_CONTROLLER)]
+    [ApiController]
+    public class OrgUnitController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<DataSet> Get([FromQuery] OrgUnit org)
+        {
+            try
+            {
+                org.Find(Database.Instance);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return NotFound(org);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] OrgUnit org)
+        {
+            try
+            {
+                var result = org.Save(Database.Instance);
+                return Created("",org);
+            }
+            catch (Exception e)
+            {
+
+                return NotFound(org);
+            }
+
+        }
+        [HttpPut]
+        public ActionResult Put([FromBody] OrgUnit org)
+        {
+            try
+            {
+                org.Update(Database.Instance);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return NotFound(org);
+            }
+
+        }
+        [HttpDelete]
+        public ActionResult Delete([FromQuery] OrgUnit org)
+        {
+            try
+            {
+                org.Delete(Database.Instance);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return NotFound(org);
+            }
+
+        }
+    }
+}
---
+++ src/Parbat.Core.Services/ParbatCore/Models/OrgUnit.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Parbat.Data;
+
+namespace ParbatCore.Models
+{
+    public class OrgUnit : IBussinesObject
+    {
+        public int unitID { get; set; }
+        public int? unitTypeID { get; set; }
+        public int? parentTypeID { get; set; }
+        public string unitName { get; set; }
+
+
+        public int Save(IDatabase db)
+        {
+            DbConnection con = db.CreateConnection();
+            DbCommand cmd = db.CreateSPCommand("spInsertOrgUnit", con);
+            cmd.Parameters.Add(db.CreateParameter(cmd, "name", this.unitName));
+            cmd.Parameters.Add(db.CreateParameter(cmd, "unitTypeID", this.unitTypeID));
+            cmd.Parameters.Add(db.CreateParameter(cmd, "parentunitID", this.parentTypeID));
+
+            con.Open();
+            var result = cmd.ExecuteScalar();
+            if (result != null)
+                this.unitID = Convert.ToInt32(result);
+
+            return this.unitID;
+        }
+
+
+        public IBussinesObject Find(IDatabase db)
+        {
+            //DbConnection con = db.CreateConnection();
+            //DbCommand cmd = db.CreateSPCommand("spInsertOrgUnit", con);
+            //cmd.Parameters.Add(db.CreateParameter(cmd, "name", this.unitName));
+            //cmd.Parameters.Add(db.CreateParameter(cmd, "unitTypeID", this.unitTypeID));
+            //cmd.Parameters.Add(db.CreateParameter(cmd, "parentunitID", this.parentTypeID));
+
+            //con.Open();
+            //DataSet result = cmd.();
+            //if (result != null)
+            //    this.unitID = Convert.ToInt32(result);
+
+            return null;
+        }
+
+
+        public int Delete(IDatabase db)
+        {
+            DbConnection con = db.CreateConnection();
+            DbCommand cmd = db.CreateSPCommand("spDeleteOrgUnit", con);
+            cmd.Parameters.Add(db.CreateParameter(cmd, "id", this.unitID));
+
+            con.Open();
+            var result = cmd.ExecuteScalar();
+            if (result != null)
+                this.unitID = Convert.ToInt32(result);
+
+            return this.unitID;
+        }
+
+        public int Update(IDatabase db)
+        {
+            DbConnection con = db.CreateConnection();
+            DbCommand cmd = db.CreateSPCommand("spUpdateOrgUnit", con);
+            cmd.Parameters.Add(db.CreateParameter(cmd, "id", this.unitID));
+            cmd.Parameters.Add(db.CreateParameter(cmd, "name", this.unitName));
+
+            con.Open();
+            var result = cmd.ExecuteScalar();
+            if (result != null)
+                this.unitID = Convert.ToInt32(result);
+
+            return this.unitID;
+        }
+
+        public DataSet GetAll(IDatabase db)
+        {
+            DataSet ds = new DataSet();
+            return ds;
+        }
+    }
+}
