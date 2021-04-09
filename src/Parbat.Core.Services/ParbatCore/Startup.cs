66f3ba3ef589248770c21f2790cc3b3129bb987b
mqpasta <mqpasta@gmail.com>
2021-04-09 21:07:25 +0500

    xml file test

--- src/Parbat.Core.Services/ParbatCore/Startup.cs
+++ src/Parbat.Core.Services/ParbatCore/Startup.cs
@@ -54,9 +54,9 @@ namespace ParbatCore
                 });
 
                 // Set the comments path for the Swagger JSON and UI.
-                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                //c.IncludeXmlComments(xmlPath);
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
 
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
