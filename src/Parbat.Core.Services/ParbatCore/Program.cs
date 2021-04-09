1eedbc1eabd7ec2394ec3578c5a3747148849d35
mqpasta <mqpasta@gmail.com>
2021-04-09 20:38:22 +0500

    =

--- src/Parbat.Core.Services/ParbatCore/Program.cs
+++ src/Parbat.Core.Services/ParbatCore/Program.cs
@@ -16,7 +16,7 @@ namespace ParbatCore
         {
             CreateWebHostBuilder(args)
                 .UseKestrel()
-                .UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"))
+                //.UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"))
                 .Build().Run();
         }
 
