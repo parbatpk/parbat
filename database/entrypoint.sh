574c65f742d769ed92120abc7a55fd5013bdca8e
mqpasta <mqpasta@DESKTOP-1IN5GU6.localdomain>
2021-04-12 01:15:06 +0500

    adding .dockerignore

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set -e
 
-/run-script.sh & /opt/mssql/bin/sqlservr 
+echo "$1"
 
+if [ ! -f tmp/app-initialized ]; then
+  function initialize_app_database(){
+     sleep 15s
+
+     /run-script.sh & /opt/mssql/bin/sqlservr
+
+     touch tmp/app-initialized
+  } 
+  initialize_app_database &
+fi
+
+# /run-script.sh & /opt/mssql/bin/sqlservr 
+
+exec "$@"
