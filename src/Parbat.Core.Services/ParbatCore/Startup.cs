ae4819ad50cd04ea9741410d1918ff41f775d658
Muhammad Qasim Pasta <mqpasta@gmail.com>
2021-04-18 22:59:15 +0500

    fixing #48 (#49)

--- src/Parbat.Core.Services/ParbatCore/Startup.cs
+++ src/Parbat.Core.Services/ParbatCore/Startup.cs
@@ -49,20 +49,20 @@ namespace ParbatCore
             {
                 c.SwaggerDoc(GlobalConstants.API_VER, new OpenApiInfo
                 {
-                    Version = GlobalConstants.API_VER,
-                    Title = "Parbat",
-                    Description = "An open source ERP for Universities",
-                    TermsOfService = new Uri("https://parbat.irada.pk/terms"),
+                    Version = "v1",
+                    Title = "Parbat Core Web API",
+                    Description = "open source ERP for universities",
+                    TermsOfService = new Uri("https://example.com/terms"),
                     Contact = new OpenApiContact
                     {
-                        Name = "Dr. Muhammad Qasim Pasta",
-                        Email = "mqpasta@irada.pk",
+                        Name = "Muhammad Qasim Pasta",
+                        Email = string.Empty,
                         Url = new Uri("https://twitter.com/mqpasta"),
                     },
                     License = new OpenApiLicense
                     {
-                        Name = "Use under LICX",
-                        Url = new Uri("https://example.com/license"),
+                        Name = "The MIT License (MIT)",
+                        Url = new Uri("http://opensource.org/licenses/mit-license.php"),
                     }
                 });
 
@@ -72,7 +72,26 @@ namespace ParbatCore
                 c.IncludeXmlComments(xmlPath);
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddMvc().
+                SetCompatibilityVersion(CompatibilityVersion.Version_2_1).
+                AddJsonOptions(o =>
+                {
+                    if (o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver
+                            as DefaultContractResolver;
+                        castedResolver.NamingStrategy = null;
+                    }
+                });
+
+
         }
 
         /// <summary>
@@ -82,6 +101,8 @@ namespace ParbatCore
         /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
@@ -113,23 +134,5 @@ namespace ParbatCore
         }
 
 
-    }
-
-    /// <summary>
-    /// To avoid naming convention in json objects
-    /// </summary>
-    public class MyTransparentJsonNamingPolicy : JsonNamingPolicy
-    {
-        /// <summary>
-        /// You can came up any custom transformation here, so instead just transparently
-        /// pass through the original C# class property name, it is possible to explicit
-        /// convert to PascalCase, etc:
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public override string ConvertName(string name)
-        {
-            return name;
-        }
     }
 }
