74c7616fcb434215800ffcfab5b4bd339b86e560
maazk9119 <maazk9119@gmail.com>
2021-04-22 17:34:14 +0500

    i have added faculty services Task 43

---
+++ src/Parbat.Core.Services/ParbatCore/Controllers/FacultyController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+using ParbatCore.Models;
+using Parbat.Data;
+namespace ParbatCore.Controllers
+{
+    /// <summary>
+    /// Handle http req of faculty Business class
+    /// </summary>
+    [Route(GlobalConstants.API_CONTROLLER)]
+    [ApiController]
+    public class FacultyController : ControllerBase
+    {
+
+        /// <summary>
+        /// Find the faculty memeber
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<Faculty> Get(long id)
+        {
+            Faculty FF = new Faculty
+            {
+                FacultyID = id
+            };
+            FF = FF.Find(Database.Instance) as Faculty;
+
+            if (FF != null)
+                return Ok(FF);
+            else
+                return NotFound();
+        }
+
+
+        /// <summary>
+        /// Return all the faculty members
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult List()
+        {
+            Faculty FF = new Faculty();
+            return Ok(FF.GetAll(Database.Instance));
+
+        }
+
+        /// <summary>
+        /// Update the info of faculty member
+        /// </summary>
+        /// <param name="faculty"></param>
+     
+        /// <returns></returns>
+        [HttpPut]
+        public ActionResult Update([FromBody]Faculty faculty)
+        {
+            if (faculty.FacultyID > 0)
+            {
+                try
+                {
+                    faculty.Update(Database.Instance);
+                    return NoContent();
+                }
+                catch (Exception e)
+                {
+
+                    return BadRequest(e.Message);
+                }
+            }
+            else
+                return BadRequest();
+
+        }
+
+
+        /// <summary>
+        /// Add new faculty member
+        /// </summary>
+        /// <param name="faculty"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<Faculty> Create([FromBody]Faculty faculty)
+        {
+            faculty.Save(Database.Instance);
+            return Created("Get", faculty);
+        }
+
+
+
+        /// <summary>
+        /// Delete the faculty member
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            Faculty FF = new Faculty
+            {
+                FacultyID = id
+            };
+
+            try
+            {
+                FF.Delete(Database.Instance);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+
+    }
+}
---
+++ src/Parbat.Core.Services/ParbatCore/Models/Faculty.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using Parbat.Data;
+using System.Data.Common;
+using System.Text.Json;
+
+namespace ParbatCore.Models
+{
+    /// <summary>
+    /// Business Model of Faculty
+    /// </summary>
+    public class Faculty:IBussinesObject
+    {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        public long FacultyID;
+        /// <summary>
+        /// Firstname 
+        /// </summary>
+        public string FirstName;
+        /// <summary>
+        /// Lastname
+        /// </summary>
+        public string LastName;
+
+        /// <summary>
+        /// Delete the Faculty member
+        /// </summary>
+        /// <param name="db"></param>
+        public void Delete(IDatabase db)
+        {
+            if(this.Find(db) == null)
+            {
+                throw new BOException("Record doesn't Exist");
+            }
+            using (DbConnection connection = db.CreateConnection())
+            {
+                connection.Open();
+                DbCommand cmd = db.CreateSPCommand(ProcedureNames.Faculty.Delete, connection);
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FacultyID", this.FacultyID));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FirstName", this.FirstName));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "LastName", this.LastName));
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Find the faculty member
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public IBussinesObject Find(IDatabase db)
+        {
+            using (DbConnection con = db.CreateConnection())
+            {
+                con.Open();
+                DbCommand cmd = db.CreateSPCommand(ProcedureNames.Faculty.Find, con);
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FacultyID", this.FacultyID));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FirstName", this.FirstName));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "LastName", this.LastName));
+                string txt = Convert.ToString(cmd.ExecuteScalar());
+                try
+                {
+                    Faculty found = JsonSerializer.Deserialize<Faculty>(txt);
+                    return found;
+                }
+                catch (JsonException je)
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all the faculty members
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public DataTable GetAll(IDatabase db)
+        {
+            using (DbConnection con = db.CreateConnection())
+            {
+                con.Open();
+                DbCommand cmd = db.CreateSPCommand(ProcedureNames.Faculty.GetAll, con);
+                DataSet ds = db.GetDataSet(cmd);
+
+                return ds.Tables[0];
+            }
+        }
+
+        /// <summary>
+        /// Save the new Faculty member if exist
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public long? Save(IDatabase db)
+        {
+            if(this.FacultyID > 0)
+            {
+                this.Update(db);
+                return this.FacultyID;
+            }
+            using (DbConnection con = db.CreateConnection())
+            {
+                con.Open();
+                DbCommand cmd = db.CreateSPCommand(ProcedureNames.Faculty.Insert,con);
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FacultyID", this.FacultyID));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FirstName", this.FirstName));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "LastName", this.LastName));
+                this.FacultyID= Convert.ToInt64(cmd.ExecuteScalar());
+                return this.FacultyID;
+            }
+
+        }
+
+        /// <summary>
+        /// Update the Faculty memeber if exist
+        /// </summary>
+        /// <param name="db"></param>
+        public void Update(IDatabase db)
+        {
+            if(this.Find(db) == null)
+            {
+                throw new BOException("Sorry, Record doesn't Exist");
+            }
+
+            using (DbConnection con = db.CreateConnection())
+            {
+                con.Open();
+                DbCommand cmd = db.CreateSPCommand(ProcedureNames.ComponentType.Update, con);
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FacultyID", this.FacultyID));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "FirstName", this.FirstName));
+                cmd.Parameters.Add(db.CreateParameter(cmd, "LastName", this.LastName));
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
