d7b0d34609536a645a6335609b7623c63efb7b75
mqpasta <mpasta@gmail.com>
2021-10-14 16:15:47 +0500

    changed dockerfile to combine SPs before compile

--- database/run-script.sh
+++ database/run-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
+#cat /sps/*.sql >combine_sps.sql
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d ParbatDB -i combine_sps.sql
 
 
