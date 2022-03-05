917e9f4ace914cd082082cf4439110355733f6fc
mqpasta <mqpasta@gmail.com>
2022-03-05 14:16:37 +0500

    new test solution and project

---
+++ src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parbat.Core.SystemTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
