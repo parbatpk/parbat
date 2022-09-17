47e70dc9d74fd3071c7c004c8d9302ae5e203bc4
maazk9119 <maazk9119@gmail.com>
2022-09-17 17:33:40 +0500

    add system test

---
+++ src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/TestCases/BatchTest.cs
@@ -0,0 +1,71 @@
+﻿using Parbat.Core.API.Client;
+using Parbat.Core.SystemTest.Base;
+
+namespace Parbat.Core.SystemTest.TestCases
+{
+    [TestClass]
+    public sealed class BatchTest 
+    {
+        [TestMethod]
+        public void Batch_Create_Valid()
+        {
+            //Arrange
+            
+            Batch batch = new Batch
+            {
+
+
+            }; 
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Create_InValid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Update_Valid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Update_InValid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Find_Valid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Find_InValid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Delete_Valid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_Delete_InValid()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Batch_GetAll_Valid()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/TestCases/ComponentTypeTest.cs
@@ -0,0 +1,87 @@
+﻿using Parbat.Core.API.Client;
+using Parbat.Core.SystemTest.Base;
+
+namespace Parbat.Core.SystemTest.TestCases
+{
+    [TestClass]
+    public class ComponentTypeTest
+    {
+
+        [TestMethod]
+        public void ComponentType_Create()
+        {
+            //arrange
+            BaseTest test = new BaseTest();
+
+            ComponentType componentType = new ComponentType
+            {
+                ComponentTypeID = -1,
+                Name = "TestingCompoenet",
+                IsRequired = true,
+            };
+
+            //act
+            test.client.ComponentTypeCreateAsync(componentType).Wait();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ComponentType_Update()
+        {
+            //arrange
+            BaseTest test = new BaseTest();
+
+            ComponentType componentType = new ComponentType
+            {
+                ComponentTypeID = -1,
+                Name = "TestingCompoenetUpdate",
+                IsRequired = false,
+            };
+
+            //act
+            var response = test.client.ComponentTypeCreateAsync(componentType).Result;
+            test.client.ComponentTypeUpdateAsync(response).Wait();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void ComponentType_Find()
+        {
+            //arrange
+            BaseTest test = new BaseTest();
+
+            //act
+            test.client.ComponentTypeGetByIdAsync(-1).Wait();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void ComponentType_Delete()
+        {
+            //arrange
+            BaseTest test = new BaseTest();
+
+            ComponentType componentType = new ComponentType
+            {
+                ComponentTypeID = -1,
+                Name = "TestingCompoenetUpdate",
+                IsRequired = false,
+            };
+
+            //act
+            var response = test.client.ComponentTypeCreateAsync(componentType).Result;
+            test.client.ComponentTypeDeleteByIdAsync(response.Key).Wait();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+    }
+}
