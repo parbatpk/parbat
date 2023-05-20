e47bec548283b52aff40b48a2e684df41d85be37
maazk9119 <maazk9119@gmail.com>
2023-05-20 19:02:54 +0500

    Client model courseOrg

---
+++ src/Parbat.Core/Parbat.Core.Web/Models/CourseOrg.cs
@@ -0,0 +1,14 @@
+﻿using Parbat.Core.API.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parbat.Core.Web.Models
+{
+    public class CourseOrg
+    {
+        [Required]
+        public Course course { get; set; }
+
+        [Required]
+        public ICollection<OrgUnit> orgUnits { get; set; }
+    }
+}
