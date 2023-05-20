981e57a09cfb2149893cf87878b7bbc8c8073d2e
maazk9119 <maazk9119@gmail.com>
2023-05-20 19:14:33 +0500

    Upgrade system test on new architecture

--- src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/TestCases/BatchTest.cs
+++ src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/TestCases/BatchTest.cs
@@ -4,67 +4,164 @@ using Parbat.Core.SystemTest.Base;
 namespace Parbat.Core.SystemTest.TestCases
 {
     [TestClass]
-    public sealed class BatchTest 
+    public class BatchTest : BaseTest
     {
         [TestMethod]
-        public void Batch_Create_Valid()
+        public void Batch_Create()
         {
-            //Arrange
-            
-            Batch batch = new Batch
+            //arrange
+            Batch obj1 = new Batch
             {
+                BatchID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName",
+                AdmissionYear = 2023,
+                GraduationYear = 2027,
+                OrgUnitID = 1,
+                CurriculumID = 1
+            };
+
+            OrgUnit obj2 = new OrgUnit
+            {
+                OrgUnitID = 0,
+                OrgUnitTypeID = 0,
+                ShortName = "TestOrgShortName",
+                Name = "TestName",
+                ParentUnitID = null,
+                IsAllowAdmission = true
+            };
+
+            OrgUnitType obj3 = new OrgUnitType
+            {
+                OrgUnitTypeID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName"
+            };
+
+            //act
+            var orgType = this.client.OrgUnitTypeCreateAsync(obj3).Result;
+            obj2.OrgUnitTypeID = orgType.Key;
+            var org = this.client.OrgUnitCreateAsync(obj2).Result;
+            obj1.OrgUnitID = org.Key;
+            var response = this.client.BatchCreateAsync(obj1).Result;
 
 
-            }; 
-
+            //assert
+            Assert.AreNotEqual(notExpected: obj1.BatchID, actual: response.BatchID);
             Assert.IsTrue(true);
         }
 
+
         [TestMethod]
-        public void Batch_Create_InValid()
+        public void Batch_Update()
         {
+            //arrange
+            Batch obj1 = new Batch
+            {
+                BatchID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName",
+                AdmissionYear = 2023,
+                GraduationYear = 2027,
+                OrgUnitID = 1,
+                CurriculumID = 1
+            };
+
+            OrgUnit obj2 = new OrgUnit
+            {
+                OrgUnitID = 0,
+                OrgUnitTypeID = 0,
+                ShortName = "TestOrgShortName",
+                Name = "TestName",
+                ParentUnitID = null,
+                IsAllowAdmission = true
+            };
+
+            OrgUnitType obj3 = new OrgUnitType
+            {
+                OrgUnitTypeID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName"
+            };
+
+            //act
+            var orgType = this.client.OrgUnitTypeCreateAsync(obj3).Result;
+            obj2.OrgUnitTypeID = orgType.Key;
+            var org = this.client.OrgUnitCreateAsync(obj2).Result;
+            obj1.OrgUnitID = org.Key;
+            var response = this.client.BatchCreateAsync(obj1).Result;
+            this.client.BatchUpdateAsync(response);
+
+            //assert
+            Assert.AreNotEqual(notExpected: obj1.BatchID, actual: response.BatchID);
             Assert.IsTrue(true);
         }
 
+
         [TestMethod]
-        public void Batch_Update_Valid()
+        public void Batch_Find()
         {
+            //act 
+            this.client.BatchGetByIdAsync(-1);
+
+            //assert
             Assert.IsTrue(true);
         }
 
+
         [TestMethod]
-        public void Batch_Update_InValid()
+        public void Batch_Delete()
         {
+            //arrange
+            Batch obj1 = new Batch
+            {
+                BatchID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName",
+                AdmissionYear = 2023,
+                GraduationYear = 2027,
+                OrgUnitID = 1,
+                CurriculumID = 1
+            };
+
+            OrgUnit obj2 = new OrgUnit
+            {
+                OrgUnitID = 0,
+                OrgUnitTypeID = 0,
+                ShortName = "TestOrgShortName",
+                Name = "TestName",
+                ParentUnitID = null,
+                IsAllowAdmission = true
+            };
+
+            OrgUnitType obj3 = new OrgUnitType
+            {
+                OrgUnitTypeID = 0,
+                ShortName = "TestShortName",
+                Name = "TestName"
+            };
+
+            //act
+            var orgType = this.client.OrgUnitTypeCreateAsync(obj3).Result;
+            obj2.OrgUnitTypeID = orgType.Key;
+            var org = this.client.OrgUnitCreateAsync(obj2).Result;
+            obj1.OrgUnitID = org.Key;
+            var response = this.client.BatchCreateAsync(obj1).Result;
+            this.client.BatchDeleteByIdAsync(response.Key);
+
+            //assert
+            Assert.AreNotEqual(notExpected: obj1.BatchID, actual: response.BatchID);
             Assert.IsTrue(true);
         }
 
-        [TestMethod]
-        public void Batch_Find_Valid()
-        {
-            Assert.IsTrue(true);
-        }
 
         [TestMethod]
-        public void Batch_Find_InValid()
+        public void Batch_GetAll()
         {
-            Assert.IsTrue(true);
-        }
+            //act
+            this.client.BatchListAsync();
 
-        [TestMethod]
-        public void Batch_Delete_Valid()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [TestMethod]
-        public void Batch_Delete_InValid()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [TestMethod]
-        public void Batch_GetAll_Valid()
-        {
+            //assert
             Assert.IsTrue(true);
         }
     }
---
+++ src/Parbat.Core/Parbat.Core.Test/Parbat.Core.SystemTest/TestCases/CourseTypeTest.cs
@@ -0,0 +1,88 @@
+﻿using Parbat.Core.API.Client;
+using Parbat.Core.SystemTest.Base;
+
+namespace Parbat.Core.SystemTest.TestCases
+{
+    [TestClass]
+    public class CourseTypeTest : BaseTest
+    {
+        [TestMethod]
+        public void CourseType_Create()
+        {
+            //arrange
+            CourseType obj = new CourseType
+            {
+                CourseTypeID = 0,
+                Name = "TestingCourseType"    
+            };
+
+            //act
+            var response = this.client.CourseTypeCreateAsync(obj).Result;
+
+            //assert
+            Assert.AreNotEqual(notExpected: obj.CourseTypeID, actual: response.CourseTypeID);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void CourseType_Update()
+        {
+            //arrange
+            CourseType obj = new CourseType
+            {
+                CourseTypeID = 0,
+                Name = "TestingCourseType"
+            };
+
+            //act
+            var response = this.client.CourseTypeCreateAsync(obj).Result;
+            this.client.CourseTypeUpdateAsync(response).Wait();
+
+            //assert
+            Assert.AreNotEqual(notExpected: obj.CourseTypeID, actual: response.CourseTypeID);
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void CourseType_Find()
+        {
+            //act
+            this.client.ComponentTypeGetByIdAsync(-1).Wait();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void CourseType_Delete()
+        {
+            //arrange
+            CourseType obj = new CourseType
+            {
+                CourseTypeID = 0,
+                Name = "TestingCourseType"
+            };
+
+            //act
+            var response = this.client.CourseTypeCreateAsync(obj).Result;
+            this.client.CourseTypeDeleteByIdAsync(response.CourseTypeID.Value).Wait();
+
+            //assert
+            Assert.AreNotEqual(notExpected: obj.CourseTypeID, actual: response.CourseTypeID);
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void CourseType_GetAll()
+        {
+            //act
+            this.client.CourseTypeListAsync();
+
+            //assert
+            Assert.IsTrue(true);
+        }
+    }
+}
