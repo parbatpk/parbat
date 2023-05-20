280cdfa2131ace632e5b5e068298f9061b78a9c5
maazk9119 <maazk9119@gmail.com>
2023-05-20 18:58:27 +0500

    Implement Course repository

--- src/Parbat.Core/Parbat.Core.BaseRepository/ICourseRepository.cs
+++ src/Parbat.Core/Parbat.Core.BaseRepository/ICourseRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Parbat.Core.DataObjects;
+using System.Collections.Generic;
 
 namespace Parbat.Core.BaseRepository
 {
     public interface ICourseRepository : IGenericRepository<Course>
     {
+        IEnumerable<OrgUnit> GetAllOrgUnitContainsCourse();
 
+        IEnumerable<Course> GetAllCourseSpecificOrgUnit(long ownerId);
     }
 }
